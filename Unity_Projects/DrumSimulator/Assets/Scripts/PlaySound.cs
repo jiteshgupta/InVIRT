e0310f7b65cb300eab69196469914c4b502af7a8
xLAB <xLAB>
2015-11-27 18:41:01 -0500

    Adding the drum simulator

---
+++ Unity_Projects/DrumSimulator/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour {
+
+	//public GameObject sparks;
+
+	//Volume:	 0..1
+	//Magnitude: 
+	void OnCollisionEnter(Collision col){
+
+		ContactPoint pos = col.contacts[0];
+
+		//Collider c = col.collider;
+
+		AudioSource audio = GetComponent<AudioSource>();
+		audio.Play();
+		//Instantiate (sparks, pos.point, Quaternion.identity);
+	}
+}
